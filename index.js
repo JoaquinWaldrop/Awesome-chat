77d17fda0e9b93b1b47bd3a9afc23cef381a8b13
Joaquin Molina <joaquin.waldrop@gmail.com>
2016-12-22 17:51:57 -0400

    Initial commit with awesome code

---
+++ index.js
@@ -0,0 +1,46 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/tone', function(req, res){
+  res.sendFile(__dirname + '/tone.mp3');
+});
+
+
+/*io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});*/
+
+/*io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});*/
+
+io.on('connection', function(socket){
+
+	socket.on('user connected', function(msg){
+    socket.broadcast.emit('user connected', msg);
+  });
+
+  socket.on('chat message', function(msg){
+    socket.broadcast.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function(){
+    socket.broadcast.emit('user disconnected', '');
+  });
+  
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
